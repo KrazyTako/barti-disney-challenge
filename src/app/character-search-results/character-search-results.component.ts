8553b4c6bbd807a9d5589c70d672ace16de64184
David Rodriguez <krazytako@hotmail.com>
2025-01-29 23:14:15 -0800

    search results progress

---
+++ src/app/character-search-results/character-search-results.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-search-results',
+  imports: [],
+  templateUrl: './character-search-results.component.html',
+  styleUrl: './character-search-results.component.css'
+})
+export class CharacterSearchResultsComponent {
+
+}
