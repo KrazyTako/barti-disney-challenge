17cb4e5c3384449a5dd98014fd76c556baccb556
David Rodriguez <krazytako@hotmail.com>
2025-01-29 23:14:03 -0800

    search results progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {CharacterCardComponent} from './character-card/character-card.component';
-import {DefaultCharactersComponent} from './default-characters/default-characters.component';
 import {FeaturedCharactersComponent} from './featured-characters/featured-characters.component';
 import {FooterComponent} from './footer/footer.component';
+import {FormsModule} from '@angular/forms';
+import {debounceTime, distinctUntilChanged, Observable, Subject, switchMap} from 'rxjs';
+import {DisneyCharacter, DisneyCharacterService} from './services/disney-character.service';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CharacterCardComponent, DefaultCharactersComponent, FeaturedCharactersComponent, RouterLink, FooterComponent],
+  imports: [RouterOutlet, FeaturedCharactersComponent, RouterLink, FooterComponent, FormsModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'disney-challenge wee';
+  searchText = '';
+  private searchSubject = new Subject<string>();
+
+  searchResults: Observable<DisneyCharacter[]> | null = null;
+
+  disneyCharacterService = inject(DisneyCharacterService);
 
   ngOnInit(): void {
+    this.searchResults = this.searchSubject.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(searchText => this.disneyCharacterService.filterByName(searchText))
+    );
+  }
+
+  search() {
+    console.log('search triggereed with', this.searchText);
+    this.searchSubject.next(this.searchText);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {CharacterDetailsComponent} from './character-details/character-details.component';
 import {DefaultCharactersComponent} from './default-characters/default-characters.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
+import {CharacterSearchResultsComponent} from './character-search-results/character-search-results.component';
 
 export const routes: Routes = [
   { path: '', component: DefaultCharactersComponent },
   { path: 'character-details/:characterId', component: CharacterDetailsComponent },
   { path: 'view-profile', component: UserProfileComponent },
+  { path: 'search', component: CharacterSearchResultsComponent },
 ];
